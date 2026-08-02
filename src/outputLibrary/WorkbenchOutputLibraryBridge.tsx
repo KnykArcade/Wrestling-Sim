5edf904754e144c953a31b2ed0c900ef12e4e7b9
KnykArcade <85686508+KnykArcade@users.noreply.github.com>
2026-08-02 09:09:14 -0400

    Load Workbench Output Library bridge styles

--- src/outputLibrary/WorkbenchOutputLibraryBridge.tsx
+++ src/outputLibrary/WorkbenchOutputLibraryBridge.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../output-library-bridge.css";
 import { loadPlannedShows } from "../planner/storage";
 import { loadWorkbenchUniverse } from "../workbench/storage";
 import { saveSegmentToOutputLibrary } from "./model";
